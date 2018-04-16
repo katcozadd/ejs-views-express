04e02afa8e25f32f1df1baa6aee05c2bb18b8ab5
katcozadd <krosecozadd@gmail.com>
2018-04-16 09:35:38 -0600

    added some commenting in app.js

--- app.js
+++ app.js
@@ -14,19 +14,20 @@ app.use(express.static('public'));
 app.use(logger('dev'));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-
+//Goes to homepage- static html file
 app.get('/', (request, response) => {
     response.sendFile('index.html', {root: 'public'});
 });
 
-
+//Menu Page with all of the pizzas - index.ejs
 app.get("/allpizza", function(request, response) {
 	response.render( 'index', {pizza: pizza} )
 });
 
+//Individual pages with specific pizzas in different pages by id - pizza-single.ejs
 app.get("/allpizza/:id", function(request, response) {
 	let id = request.params.id;
-	response.render( 'individual/pizza-single', pizza[id-1])
+	response.render( 'single/pizza-single', pizza[id-1])
 })
 
 /* error handler */
